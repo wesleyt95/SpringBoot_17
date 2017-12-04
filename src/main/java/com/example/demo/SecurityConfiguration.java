a2d9b451001effb604b83a5102fefc3b3625be7f
Wesley Bond <wesley.bond@icloud.com>
2017-12-04 16:20:15 -0500

    dd

---
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+    @Override
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and().httpBasic();
+    }
+}
